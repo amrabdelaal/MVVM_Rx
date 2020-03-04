bbd30e247a577c5c3e43075d17331696b9e46c45
Amr Abdelaal <amr.abdelaal@denkwerk.com>
2020-03-04 16:53:38 +0100

    Initial Commit

---
+++ MVVM_Rx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_Rx
+//
+//  Created by Amr Abdelaal on 04.03.20.
+//  Copyright © 2020 Amr Abdelaal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM_RxTests/MVVM_RxTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_RxTests.swift
+//  MVVM_RxTests
+//
+//  Created by Amr Abdelaal on 04.03.20.
+//  Copyright © 2020 Amr Abdelaal. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_Rx
+
+class MVVM_RxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
